bc418e6306620c8e50ad0ff18bd6073c1af774b5
Mario Rodriguez <mrodrigu@northseattle.edu>
2018-05-21 09:36:58 -0700

    creating a new repo of practice files for nodeJS

---
+++ notes.js
@@ -0,0 +1,10 @@
+console.log("starting note.js");
+
+// export function addNote() {
+//     console.log("addNote");
+//     return "New Note";
+// }
+
+module.exports.add = (x, y) => {
+    return x+y;
+}
