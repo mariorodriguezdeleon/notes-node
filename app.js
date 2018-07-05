fb650b326a195576107f88bc3e0ec0cbf368da7c
Mario Rodriguez <mrodrigu@northseattle.edu>
2018-07-05 13:22:47 -0700

    deleted txt file, added gitignore file, and reset for further app dev

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 console.log("starting app.js");
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-console.log(notes.add(3, 5));
-
-// newFunction();
-// function newFunction() {
-//     var res = notes.addNote();
-//     console.log(res);
-// }
-
-// var user = os.userInfo();
-// var userName = user.username;
-
-// console.log(user);
-
-// fs.appendFile("greetings.txt", `\n Hellow there, ${userName}!. You are ${notes.age} old.`, err => {
-//     if (err) throw err;
-//     console.log("data was appended to file");
-// });
--- notes.js
+++ notes.js
@@ -1,10 +1,6 @@
 console.log("starting note.js");
 
-// export function addNote() {
-//     console.log("addNote");
-//     return "New Note";
-// }
-
-module.exports.add = (x, y) => {
-    return x+y;
+export function addNote() {
+    console.log("addNote");
+    return "New Note";
 }
